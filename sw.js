308ce855f641d473579db13cbff6a5d78620eae2
omahdigital12 <omahdigital12@gmail.com>
2026-07-18 22:13:59 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'presensi-v4';
+
+const urlsToCache = [
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 🔥 INSTALL → simpan cache awal
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 🔄 ACTIVATE → hapus cache lama
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// 🌐 FETCH → ambil dari cache dulu, baru internet
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
